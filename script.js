88d913cc5d6ea847a49e553fe42bc9398f24bab4
MahmoudElsawy <141438964+MahmoudElsawyAbdelAal1@users.noreply.github.com>
2023-10-17 15:38:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let iphon = document.getElementById('iphone')
+let container = document.querySelector('.container')
+console.log(container)
+
+
+function swichIphone(src){
+
+    iphon.src = src;
+ }
+
+function swichColor(color){
+
+   container.style.backgroundColor = color;
+
+
+
+}
